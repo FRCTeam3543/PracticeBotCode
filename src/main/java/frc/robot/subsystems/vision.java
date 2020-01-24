9ae357f38f7a1b3ef0220c2d5723349a3ccf3b33
Dave-Headrick-98 <43453262+Dave-Headrick-98@users.noreply.github.com>
2020-01-24 14:11:41 -0500

    Update vision.java

--- src/main/java/frc/robot/subsystems/vision.java
+++ src/main/java/frc/robot/subsystems/vision.java
@@ -4,6 +4,7 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
+public void vision_start(){
 //initializing.
 NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
 NetworkTableEntry tx = table.getEntry("tx");
@@ -18,4 +19,5 @@ double area = ta.getDouble(0.0);
 //post to smart dashboard periodically
 SmartDashboard.putNumber("LimelightX", x);
 SmartDashboard.putNumber("LimelightY", y);
-SmartDashboard.putNumber("LimelightArea", area);
+SmartDashboard.putNumber("LimelightArea", area);  
+}
