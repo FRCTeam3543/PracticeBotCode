5d36728f8eae0411e91e65c070b72967803cb73f
Dave-Headrick-98 <43453262+Dave-Headrick-98@users.noreply.github.com>
2020-02-26 17:25:58 -0500

    Add files via upload

--- src/main/java/frc/robot/commands/XboxControlling.java
+++ src/main/java/frc/robot/commands/XboxControlling.java
@@ -4,9 +4,11 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.OI;
 import frc.robot.Robot;
+import frc.robot.subsystems.Config;
 
 public class XboxControlling extends Command {
     boolean usingArcade = false;
+    boolean usingVision = false;
 
     public void driveswitch() {
         if (OI.xbox.getBButtonPressed()) {
@@ -20,6 +22,16 @@ public class XboxControlling extends Command {
             Robot.tanky.tankDrive(OI.xbox.getY(Hand.kLeft), OI.xbox.getY(Hand.kRight));
         }
 
+    }
+
+    public void visionswitch(){
+        if (OI.xbox.getYButton()){
+            usingVision = !usingVision;
+        }
+
+        if (usingVision) {
+            Robot.tanky.visionDrive(Config.PRECISION_DRIVE_LIMIT);
+        }
     }
 
     @Override
