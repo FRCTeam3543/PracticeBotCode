d63336a30a9fe4c06402de5dd9b0ae2bd19c5548
Dave-Headrick-98 <43453262+Dave-Headrick-98@users.noreply.github.com>
2020-02-28 15:16:44 -0500

    Add files via upload
    Updated for the victors

--- src/main/java/frc/robot/commands/XboxControlling.java
+++ src/main/java/frc/robot/commands/XboxControlling.java
@@ -2,13 +2,14 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.OI;
 import frc.robot.Robot;
 import frc.robot.subsystems.Config;
 
 public class XboxControlling extends Command {
     boolean usingArcade = false;
-    boolean usingVision = false;
+
 
     public void driveswitch() {
         if (OI.xbox.getBButtonPressed()) {
@@ -17,28 +18,20 @@ public class XboxControlling extends Command {
 
         if (usingArcade) {
             Robot.tanky.arcadeDrive(OI.xbox.getY(Hand.kLeft), -OI.xbox.getX(Hand.kLeft));
+            SmartDashboard.getBoolean("UsingArcade", usingArcade);
             }
         else{
-            Robot.tanky.tankDrive(OI.xbox.getY(Hand.kLeft), OI.xbox.getY(Hand.kRight));
-        }
-
-    }
-
-    public void visionswitch(){
-        if (OI.xbox.getYButton()){
-            usingVision = !usingVision;
-        }
-
-        if (usingVision) {
             Robot.tanky.visionDrive(Config.PRECISION_DRIVE_LIMIT);
-        }else{
-            Robot.tanky.arcadeDrive(OI.xbox.getY(Hand.kLeft), -OI.xbox.getX(Hand.kLeft));
+            SmartDashboard.putString("DriveSystem", "Vision");
         }
+
     }
 
     @Override
     protected boolean isFinished() {
         return false;
     }
+
+    
 
 }
